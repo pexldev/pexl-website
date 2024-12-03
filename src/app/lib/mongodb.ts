2cd19f2f2e3a8d501d3545dbd8711e0e168b1110
Prateek Muraleedharan <prateekmuralidharan@gmail.com>
2024-12-03 10:07:50 +0530

    Add MongoDB connection fixes

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -1,6 +1,10 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || '';
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error('MONGODB_URI environment variable is not defined');
+}
+
 const options = {};
 
 let client: MongoClient;
