e6e806b1e678b330efc6afc9863fd46a6ff98390
Prateek Muraleedharan <prateekmuralidharan@gmail.com>
2024-12-08 18:48:42 +0530

    Updated files for Pexel website

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['res.cloudinary.com'], // Adding our trusted image source
+  },
   /* config options here */
 };
 
